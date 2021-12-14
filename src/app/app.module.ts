9fa9e5757ef6f2801ba63f3e48b966aa7e00cd32
darkunchain <luisfer74081@gmail.com>
2021-12-14 13:26:45 -0500

    carousel fijo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    NgxUsefulSwiperModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+
+  images = [
+    "assets/img/carousel_novena.svg",
+    "assets/img/carousel1.png",
+    "assets/img/carousel2.png",
+    "assets/img/carousel3.png",
+    "assets/img/carousel4.png"
+  ]
+
+
+  config: SwiperOptions = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30,
+    speed: 400,
+  }
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
